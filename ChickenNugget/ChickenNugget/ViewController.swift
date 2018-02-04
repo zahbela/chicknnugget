3a20949bbdafd966307af4ff30ee9d911b034ed4
Zahnae Aquino <zaquino@calpoly.edu>
2018-02-03 20:02:44 -0800

    MY NUGGETS

---
+++ ChickenNugget/ChickenNugget/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ChickenNugget
+//
+//  Created by Zahnae Aquino on 2/3/18.
+//  Copyright © 2018 Zahnae Aquino. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
